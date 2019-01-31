3f6220ead5e6c49251684dcede2d9f181f705fc7
Kevin Bowman <kevin.bowman@zenuity.com>
2019-01-31 17:19:36 -0500

    committing to git working

--- opencv_vid_test.py
+++ opencv_vid_test.py
@@ -2,12 +2,13 @@
 
 import numpy as np
 import cv2
-import time
+import os
+from time import sleep
 
 # If you are having trouble with exceptions when calling detectMultiScale
 # you may want to ensure that this path is valid
 haar_cascade_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-haar_cascade_profile = cv2.CascadeClassifier('haarcascade_profileface.xml')
+#haar_cascade_profile = cv2.CascadeClassifier('haarcascade_profileface.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -17,15 +18,10 @@ while not cap.isOpened():
 while(True):
     # capture frame by frame
     frame_ok, frame = cap.read()
-    if not frame_ok:
-        print("bad frame")
-        continue
 
-    # input("Enter...")
-
+    # haar cascades require grayscale images
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # operation on the frame ...
-    # print("trying face detection")
     # If you are having trouble with exceptions when calling detectMultiScale
     # you may want to ensure the file passed to the
     # CascaseClassifier constructor is valid and at the specified location
@@ -37,14 +33,23 @@ while(True):
     )
     for (x,y,w,h) in face_rects:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (168, 201, 97), 2)
-    face_rects = haar_cascade_profile.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
-    for (x,y,w,h) in face_rects:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (83, 85, 226), 2)
+
+    if len(face_rects):
+        cv2.imwrite('test.jpg', frame)
+        sleep(0.1)  # wait for write to complete
+        os.system('git commit test.jpg -m \"x\"')
+        os.system('git push')
+        break
+
+    # Profile detection
+    # face_rects = haar_cascade_profile.detectMultiScale(
+    #     gray,
+    #     scaleFactor=1.1,
+    #     minNeighbors=5,
+    #     minSize=(30, 30)
+    # )
+    # for (x,y,w,h) in face_rects:
+    #     cv2.rectangle(frame, (x, y), (x+w, y+h), (83, 85, 226), 2)
 
     #display captured frame
     cv2.imshow('frame', frame)
